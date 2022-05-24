b71756f90a937f8a84ea1d7c800032a59d1a24b8
obinna-dev <100476375+obinna-dev@users.noreply.github.com>
2022-05-24 21:02:21 +0100

    Set up basic JSX and CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
+import { Loader, Navbar, Footer, Welcome, Transactions, Services } from '../src/components/Index'
+ 
  function App() {
 
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer /> 
     </div>
   )
 }
